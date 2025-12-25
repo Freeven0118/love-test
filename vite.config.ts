92ef7f16f7155e65a9ea01054a6b144772f9125d
Freeven <freeven0118@gmail.com>
2025-12-25 23:32:30 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // 關鍵修正：建立橋樑
+    // 讓程式碼裡的 process.env.API_KEY 能讀取到你在 Vercel 設定的 VITE_API_KEY
+    'process.env.API_KEY': 'import.meta.env.VITE_API_KEY'
+  }
 })
